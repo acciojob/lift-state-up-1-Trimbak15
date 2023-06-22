8e3d184f9c47f8c0d903be254ae320463e07be17
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-22 12:32:53 +0000

    final

--- src/components/Child.js
+++ src/components/Child.js
@@ -15,7 +15,7 @@ const Child = ({showModal, setShowModal}) => {
             {
                 showModal && (
                     <div>
-                        <h3>Model Content</h3>
+                        <h3>Modal Content</h3>
                         <p>This is the modal content.</p>
                     </div>
                 )
